c5cce19dd93804105de08af2f39aee928b4e8c30
Stone Harris <stonetharris@gmail.com>
2023-06-04 14:17:47 -0400

    Domain issue

--- julayo/settings.py
+++ julayo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.julayo.org, julayo.org', '127.0.0.1']
+ALLOWED_HOSTS = ['julayo.herokuapp.com, www.julayo.org, julayo.org', '127.0.0.1']
 
 
 # Application definition
